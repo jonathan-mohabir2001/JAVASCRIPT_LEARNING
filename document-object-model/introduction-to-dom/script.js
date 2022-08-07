0bc062234c79d1e774bba9cb774fc43478aff81d
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-08-07 09:11:19 -0400

    DOM manipulation started, script.js file added for practise

---
+++ document-object-model/introduction-to-dom/script.js
@@ -0,0 +1,37 @@
+/*
+This Javascript File will focus on understanding the 
+document object model. 
+
+Four main nodes: 
+1. Document 
+2. Element 
+3. Attribute 
+4. Text 
+
+These will be focused on as to how to access and use these nodes. 
+
+The html document linked to this JS file has elements with IDs 
+attached to them in order to be worked with to understand DOM
+*/
+
+var firstItem = document.getElementById('one'); 
+/*
+The statement above is "catching" the first element 
+from the list. 
+
+Because the element will be worked on more than once 
+it is assigned to that variable name. 
+
+Remember, 
+
+to use these methods the parent node DOCUMENT 
+must first be called, and then dot notation is used to 
+work with elemental nodes. 
+
+The id Value of the first item from the list is passed 
+in quotes inside the brackets. 
+
+*/
+
+
+
