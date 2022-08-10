966dff699987cd6ecc854fe7c69e2255b0546cd0
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-08-10 18:56:25 -0400

    DOM understanding, file added, just comments

--- document-object-model/introduction-to-dom/script.js
+++ document-object-model/introduction-to-dom/script.js
@@ -1,36 +1,13 @@
 /*
-This Javascript File will focus on understanding the 
-document object model. 
+This JS file will focus the document object model for the first time. 
 
-Four main nodes: 
-1. Document 
-2. Element 
-3. Attribute 
-4. Text 
+The dom is not JS or HTML, it is like a set of rules which 
+dictates how HTML is modeled onto a page, and how JS can access and 
+updates contents of that page. 
 
-These will be focused on as to how to access and use these nodes. 
-
-The html document linked to this JS file has elements with IDs 
-attached to them in order to be worked with to understand DOM
-*/
-
-var firstItem = document.getElementById('one'); 
-/*
-The statement above is "catching" the first element 
-from the list. 
-
-Because the element will be worked on more than once 
-it is assigned to that variable name. 
-
-Remember, 
-
-to use these methods the parent node DOCUMENT 
-must first be called, and then dot notation is used to 
-work with elemental nodes. 
-
-The id Value of the first item from the list is passed 
-in quotes inside the brackets. 
-
+HTML model: ("DOM TREE")
+4 main nodes of DOM tree: Document, Element, Attribute, Text 
+JS manipulation: Own methods and properties to access and change objects from the tree. 
 */
 
 
