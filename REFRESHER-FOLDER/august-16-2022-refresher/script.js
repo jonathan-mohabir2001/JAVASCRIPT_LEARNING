a41c8473cc596bdaaed1ce94b9b5228ceb7d2983
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-08-16 19:02:09 -0400

    new refresher folder added, revision of past material

---
+++ REFRESHER-FOLDER/august-16-2022-refresher/script.js
@@ -0,0 +1,34 @@
+/*
+This JS file is only review for what I remember from pracitising since July 
+*/
+
+// SIMPLE VARIABLES BELOW 
+var myName; 
+var myAge; 
+var myBirthYear; 
+
+// ASSINGING VALUES TO VARIABLES 
+myName = 'Jonathan Mohabir'; 
+myAge = 20; 
+myBirthYear = 2001; 
+
+// ARRAYS below in literal and constructor notation  
+var myFavNumbers = [10,10,2001]; 
+var favLetters = new Array ('A' , 'B' , 'C'); 
+
+// OBJECTS in literal and constructor notation below 
+
+var jonathanObject = {
+  givenName: 'Jonathan M' , 
+  dateOfBirth: 2001, 
+  placeOfBirth: 'Jane and Finch, Toronto ON'
+}; 
+
+var jonathanSecondObject = new Object(); 
+jonathanSecondObject.name = 'Jonathan Mohabir'; 
+
+
+alert(jonathanObject.givenName); 
+
+
+
