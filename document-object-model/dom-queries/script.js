5b3c5f355f451012fb33b64ce465b182c959b8a5
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-08-16 19:50:38 -0400

    DOM FOLDER ADDED, dom queries introduced

---
+++ document-object-model/dom-queries/script.js
@@ -0,0 +1,20 @@
+/*
+This Javascript File will focus on the Document Object Model
+
+Understanding the DOM, is to understand how to work with the DOM TREE. 
+
+*/
+
+// TO WORK WITH NODES AND THEIR ELEMENTS, DOM QUERIRES NEED TO BE MADE 
+git 
+document.getElementById('one'); 
+document.getElementById('two'); 
+document.getElementById('three'); 
+document.getElementById('four'); 
+
+// THESE DOM QUERIES ONLY CATCH THESE IDs ONLY ONCE 
+
+// DOM QUERIES WILL BE ASSIGNED TO VARIABLES TO WORK WITH THAT ELEMENT MORE THAN ONCE 
+
+var mainPage = document.getElementById('page'); 
+
