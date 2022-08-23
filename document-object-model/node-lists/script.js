47a7a5c658183f03af02606a7ddf870d6ccb2215
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-08-23 19:46:34 -0400

    DOM learning continued, working with nodelists added

--- document-object-model/node-lists/script.js
+++ document-object-model/node-lists/script.js
@@ -4,3 +4,10 @@ This JAVASCRIPT file will focus on node lists.
 A second aspect to document object model, which can return more than one element.
 */
 
+
+var listElements = document.getElementsByTagName('li'); 
+document.write(listElements.length); 
+
+// A nodelist query is created by getting  all the li elements
+
+// The length of the elements are then written out to the DOM. 
