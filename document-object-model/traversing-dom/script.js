b9b5f5407ef75794569f71ef4551d906f51998ec
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-08-28 08:39:04 -0400

    Traversing DOM folder added

---
+++ document-object-model/traversing-dom/script.js
@@ -0,0 +1,14 @@
+/*
+AUG-28th-2022
+
+THIS JAVASCRIPT FILE will contain practise code of traversing the DOM 
+
+Five basic aspects/properties are: 
+parentNode,
+
+previousSibling
+nextSibling, 
+
+firstChild 
+lastChild
+*/
