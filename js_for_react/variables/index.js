c49d475d8d75c140544cf130e9a5ee91a81a27fd
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-10-03 19:10:13 -0400

    Modern javascript for react will be covered in this repo, code added to this folder from the chapter 02

---
+++ js_for_react/variables/index.js
@@ -0,0 +1,13 @@
+/*
+This file will contain variable practise for the latest 
+practises with Javascript in order to work with react. 
+
+ES6 and after is the Javascript that will be used to work 
+with this library. 
+*/
+// prior to ES6 the only way to declare a variable was with var 
+const myName = "Jonathan Mohabir"
+const myAge = 20; 
+// these are two variables that cannot be changed, const must be used. 
+// myAge = 23 this statement will cause an error. 
+//LEXICAL SCOPE EXAMPLE 
