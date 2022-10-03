6f3906ff518aefc06545f2cd025621f49544e3a9
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-10-03 10:29:19 -0400

    callback functions practised

---
+++ promises_review/callbacks/index.js
@@ -0,0 +1,38 @@
+/*
+This javascript file will cover a review of callbacks 
+
+Call backs are functions which are passed as arugments 
+into other arguments.Below will be code to demonstrate. 
+
+This file is initated as a node file. 
+
+*/
+
+function sayGreeting(message){
+  console.log(`Here is a following message: ${message}`)
+} 
+// this function is passed a parameter to hold a message. 
+
+function processMessage(callback){
+  // in the parameter callback is passed in order to call back... the say something constant delcared below 
+  const saySomething = "Here is a message!"
+
+  callback(saySomething)
+  // callback is passed the variable of saySomething
+};
+processMessage(sayGreeting);
+// the processMessage is passed a parameter of the say greeting function which holds the message variable. 
+
+// below will be another example demonstrating callbacks
+
+function doSomeMath(output){
+  console.log(`Here is an output function: ${output}`)
+}
+// this function will hold one output which will allow for holding operations in the other 
+function processOutput (callback){
+  const firstOutput = 1+1 
+  callback(firstOutput)
+  const secondOutput = 2+34
+  callback(secondOutput)
+}
+processOutput(doSomeMath)
