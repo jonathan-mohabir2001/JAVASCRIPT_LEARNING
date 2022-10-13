1c5752c8ea6c9714712bcf5d14795171b26efdee
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-10-13 14:47:11 -0400

    ES6 concepts covered are template_strings, and variables

---
+++ react_js/template_strings/index.js
@@ -0,0 +1,23 @@
+/*
+This js file will cover template strings. 
+
+${}, is used to access variables through their values. 
+*/
+const myName = 'Jonathan M';
+const myAge = 21;
+const myBirthYear = 2001;
+
+console.log(
+`Hi there my name is ${myName}, and my age is ${myAge} and I was born 
+in the year of ${myBirthYear}`
+);
+// above are three constants declared in the constant format. 
+
+/*
+The template strings give us flexbility to write variables with 
+assigned values and then access them through 
+*/
+const number1 = 19.00
+const number2 = 54.80
+
+console.log(`My earning rate per hour was only $${number1} and I INTEND, AND WILL move to this per hour $${number2} starting APRIL 2023`)
--- react_js/variables/index.js
+++ react_js/variables/index.js
@@ -1,4 +1,16 @@
 /*
 Varibale practise for working with ES6 concepts 
 
+Before 2015, the only way to make varibales within js 
+was to use the var keyword. 
 */
+// looking at const...this was introduced with ES6 
+var pizza = true;
+console.log(pizza)
+pizza = false; 
+console.log(pizza); 
+// these statements are interchangable with their values because the var keyword is used. 
+const random = true; 
+//random = false
+//the above statement causes an error as the constant value cannot be changed. 
+
